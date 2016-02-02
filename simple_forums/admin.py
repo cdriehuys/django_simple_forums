2a529c8e1a018c37d70c02b42b10d09cd4a9b51e
Chathan Driehuys <cdriehuys@gmail.com>
2016-02-01 23:01:39 -0500

    Registering Message model in admin.

--- simple_forums/admin.py
+++ simple_forums/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from simple_forums import models
 
 
+class MessageAdmin(admin.ModelAdmin):
+    """ Admin for the Message model """
+
+    class Meta:
+        model = models.Message
+        fields = ('user',)
+
+
 class ThreadAdmin(admin.ModelAdmin):
     """ Admin for the Thread model """
 
@@ -11,4 +19,5 @@ class ThreadAdmin(admin.ModelAdmin):
         fields = ('title',)
 
 
+admin.site.register(models.Message, MessageAdmin)
 admin.site.register(models.Thread, ThreadAdmin)
