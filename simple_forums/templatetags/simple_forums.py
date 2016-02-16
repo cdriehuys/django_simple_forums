7bfd23479c104acf785f578e72d761cb7258fe42
Chathan Driehuys <cdriehuys@gmail.com>
2016-02-16 17:04:29 -0500

    Rendering messages using markdown.

--- simple_forums/templatetags/simple_forums.py
+++ simple_forums/templatetags/simple_forums.py
@@ -1,6 +1,9 @@
 from django import template
 from django.core.urlresolvers import reverse
 from django.template.defaultfilters import urlencode
+from django.utils.safestring import mark_safe
+
+from simple_forums.markup_renderers import markdown
 
 
 register = template.Library()
@@ -22,3 +25,11 @@ def logout_url(context):
     return '%s?next=%s' % (
         reverse('logout'),
         urlencode(request.path))
+
+
+@register.simple_tag
+def render_markup(text):
+    """ Render text using the renderer specified in settings.py """
+    renderer = markdown.MarkdownRenderer()
+
+    return mark_safe(renderer.render(text))
