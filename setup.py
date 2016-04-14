be719ce9e0be295fcdecf223117bee127d1f8dfb
Chathan Driehuys <cdriehuys@gmail.com>
2016-04-14 00:26:43 -0400

    Bump version: 1.2.0 → 1.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='django_simple_forums',
-    version='1.2.0',
+    version='1.3.0',
     description='A simple forums app for Django',
     long_description=readme(),
     url='http://github.com/smalls12/django_simple_forums',
