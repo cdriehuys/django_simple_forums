10557d499f2ddcc44de96877dff288cd23cec9e4
Chathan Driehuys <cdriehuys@gmail.com>
2016-11-17 16:17:10 -0500

    Create test factories for models.

--- simple_forums/tests/testing_utils.py
+++ simple_forums/tests/testing_utils.py
@@ -1,101 +1,64 @@
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-from simple_forums import models
+import factory
 
 
-def create_message(**kwargs):
-    """ Create a message instance with the given arguments.
-
-    Since this method is used for testing, it will create default
-    values to pass to the instance.
+class MessageFactory(factory.django.DjangoModelFactory):
     """
-
-    user = kwargs.pop('user', None)
-    thread = kwargs.pop('thread', None)
-    body = kwargs.pop('body', 'test body')
-    time_created = kwargs.pop('time_created', None)
-
-    if len(kwargs) > 0:
-        raise ValueError("Received unexpected kwargs: %s" % kwargs)
-
-    if user is None:
-        user = get_test_user()
-
-    if thread is None:
-        thread = create_thread()
-
-    message_kwargs = dict(
-        user=user,
-        thread=thread,
-        body=body)
-
-    # Since this field has a default value defined in the model itself,
-    # don't pass it if its value is None
-    if time_created is not None:
-        message_kwargs['time_created'] = time_created
-
-    return models.Message.objects.create(**message_kwargs)
-
-
-def create_thread(**kwargs):
-    """ Create a thread instance for testing.
-
-    Fills in default values for testing purposes.
+    Factory for creating messages.
     """
+    body = 'Test message body.'
+    thread = factory.SubFactory(ThreadFactory)
+    time_created = factory.LazyFunction(timezone.now)
+    user = factory.SubFactory(UserFactory)
 
-    topic = kwargs.pop('topic', None)
-    title = kwargs.pop('title', 'test thread')
-    sticky = kwargs.pop('sticky', None)
-    time_created = kwargs.pop('time_created', None)
-
-    if len(kwargs) > 0:
-        raise ValueError("Received unexpected kwargs: %s" % kwargs)
-
-    if topic is None:
-        topic = create_topic()
-
-    thread_kwargs = dict(
-        topic=topic,
-        title=title)
-
-    # Since these fields have a default value defined in the model
-    # itself, don't pass them if their value is None
-    if sticky is not None:
-        thread_kwargs['sticky'] = sticky
-
-    if time_created is not None:
-        thread_kwargs['time_created'] = time_created
-
-    # Create the thread with the given parameters
-    return models.Thread.objects.create(**thread_kwargs)
+    class Meta:
+        model = 'simple_forums.Message'
 
 
-def create_topic(**kwargs):
-    """ Create topic instance for testing.
-
-    Fills in default values for testing purposes.
+class ThreadFactory(factory.django.DjangoModelFactory):
     """
+    Factory for generating threads.
+    """
+    sticky = False
+    time_created = factory.LazyFunction(timezone.now)
+    title = factory.Sequence(lambda n: "Thread {n}".format(n=n))
+    topic = factory.SubFactory(TopicFactory)
 
-    title = kwargs.pop('title', 'test topic')
-    description = kwargs.pop('description', 'test description')
-
-    if len(kwargs) > 0:
-        raise ValueError("Received unexpected kwargs: %s" % kwargs)
-
-    topic_kwargs = dict(
-        title=title,
-        description=description)
-
-    return models.Topic.objects.create(**topic_kwargs)
+    class Meta:
+        model = 'simple_forums.Thread'
 
 
-def get_test_user(username='test', password='test', email=None):
-    """ Get or create user for testing purposes """
+class TopicFactory(factory.django.DjangoModelFactory):
+    """
+    Factory for generating topics.
+    """
+    description = 'Test topic body.'
+    title = factory.Sequence(lambda n: "Topic {n}".format(n=n))
 
-    if get_user_model().objects.filter(username=username).exists():
-        return get_user_model().objects.get(username=username)
+    class Meta:
+        model = 'simple_forums.Topic'
 
-    return get_user_model().objects.create_user(
-        username=username,
-        password=password,
-        email=email)
+
+class UserFactory(factory.django.DjangoModelFactory):
+    """
+    Factory for generating users.
+    """
+    email = factory.LazyAttribute(lambda obj: '{usr}@example.com'.format(
+        usr=obj.username))
+    password = 'password'
+    username = factory.Sequence(lambda n: 'user{n}'.format(n=n))
+
+    class Meta:
+        django_get_or_create = ('username',)
+        model = get_user_model()
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        """
+        Override the default ``_create`` with our custom call.
+        """
+        manager = cls._get_manager(model_class)
+
+        return manager.create_user(*args, **kwargs)
